0b599de9a5e6c2f79422e01c908fac48787a5de2
irakli-dadu <lipsowow@gmail.com>
2022-11-01 15:13:11 +0400

    aacc
    ss

---
+++ index.js
@@ -0,0 +1,48 @@
+// const cars = ["tesla", "bmw", "fiat", "audi"];
+
+// const [erti, ori] = cars;
+
+// console.log(erti, ori);
+
+
+// const player = {
+//     name: "Patrik",
+//     lname: "Linberg",
+//     age: 34
+// }
+
+// const {age, lname, name} = player;
+
+// console.log(age, lname);
+
+
+
+// let player = {
+//     name: "john",
+//     age: 33
+// }
+
+// let gamer = player;
+
+// console.log(player, gamer);
+
+
+// let player = {
+//     name: "gio",
+//     age: 20
+// }
+
+// let player2 = {...player}
+
+// player2.age = 25;
+
+// console.log(player, player2);
+
+
+// let arr1 = [1,2,3,4,5,6];
+
+// let arr2 = [...arr1];
+
+// arr2[3] = 99;
+
+// console.log(arr1, arr2);
