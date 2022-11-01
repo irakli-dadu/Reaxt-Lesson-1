d314738ce7a235291e13f439211f591e4679d983
irakli-dadu <lipsowow@gmail.com>
2022-11-01 15:26:04 +0400

    sasw
    dd

--- index.js
+++ index.js
@@ -46,3 +46,33 @@
 // arr2[3] = 99;
 
 // console.log(arr1, arr2);
+
+
+
+// Task 1 --------------------
+
+let array1 = [-3,-8,-60,1,3,6,8,10,12,-23,-87,46,66,88,99];
+
+function favnumbers(num) {
+    return num > 0
+}
+
+function sumEl(num, ind) {
+    return num + ind
+}
+
+let numbers1 = array1.filter(favnumbers);
+// console.log(numbers1);
+
+let numbersSum = array1.reduce(sumEl);
+
+// console.log(numbersSum);
+
+
+
+
+
+
+// task 2 -----------------------------------------------------
+
+
